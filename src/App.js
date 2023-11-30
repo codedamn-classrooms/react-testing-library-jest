5cbc53d82c4aa6509809ae9e6141c1c4c68a521c
Pranav <pranav.techiegeek@gmail.com>
2023-11-30 12:34:01 +0530

    ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { JokePage } from "./pages/JokePage";
 import { PageNotFound } from "./pages/PageNotFound";
 
-
 function App() {
-  return (
-      <>
-        <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="joke" element={<JokePage />}></Route>
-            <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/home" element={<HomePage />}></Route>
+				<Route path="/" element={<JokePage />}></Route>
+				<Route path="*" element={<PageNotFound />}></Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
